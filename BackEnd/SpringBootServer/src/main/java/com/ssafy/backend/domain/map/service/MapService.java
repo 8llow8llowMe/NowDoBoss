1363e58084e868361da5519ed6e1258f73ea1d78
tlsehdrms124 <tlsehdrms124@naver.com>
2024-11-13 23:46:05 +0900

    [BE] fix: 맵 좌표 관련 수정 (상권)

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/map/service/MapService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/map/service/MapService.java
@@ -3,7 +3,7 @@ package com.ssafy.backend.domain.map.service;
 import com.ssafy.backend.domain.map.dto.response.MapResponse;
 
 public interface MapService {
-    MapResponse getCommercialAreaCoords(double ax, double ay, double bx, double by) throws Exception;
-    MapResponse getAdministrationAreaCoords(double ax, double ay, double bx, double by) throws Exception;
-    MapResponse getDistrictAreaCoords(double ax, double ay, double bx, double by) throws Exception;
+    MapResponse getCommercialAreaCoords(double ax, double ay, double bx, double by);
+    MapResponse getAdministrationAreaCoords(double ax, double ay, double bx, double by);
+    MapResponse getDistrictAreaCoords(double ax, double ay, double bx, double by);
 }
