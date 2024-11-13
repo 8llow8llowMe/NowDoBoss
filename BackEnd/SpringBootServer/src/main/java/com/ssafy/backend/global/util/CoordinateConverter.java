4c5fd6a692fb2f68a35f96863a4c05684e63875e
신동근 <tlsehdrms124@naver.com>
2024-11-13 09:54:06 +0900

    [BE] fix: 행정동 및 상권지역 관련 geotools를 이용한 좌표 변환 리팩토링 진행

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/util/CoordinateConverter.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/util/CoordinateConverter.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend.global.util;
 
+import com.ssafy.backend.domain.commercial.exception.CoordinateTransformationException;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.referencing.CRS;
@@ -12,18 +13,27 @@ import org.opengis.referencing.operation.MathTransform;
 
 
 public class CoordinateConverter {
+
+    private static final GeometryFactory GEOMETRY_FACTORY = JTSFactoryFinder.getGeometryFactory();
+    private static final CoordinateReferenceSystem SOURCE_CRS;
+    private static final CoordinateReferenceSystem TARGET_CRS;
+    private static final MathTransform TRANSFORM;
+
+    static {
+        try {
+            SOURCE_CRS = CRS.decode("EPSG:5181");
+            TARGET_CRS = CRS.decode("EPSG:4326");
+            TRANSFORM = CRS.findMathTransform(SOURCE_CRS, TARGET_CRS, true);
+        } catch (Exception e) {
+            throw new CoordinateTransformationException("좌표 변환 초기화 실패", e);
+        }
+    }
+
     public static Point transform(double x, double y) throws Exception {
-        CoordinateReferenceSystem sourceCrs = CRS.decode("EPSG:5181");
-        CoordinateReferenceSystem targetCrs = CRS.decode("EPSG:4326");
-
-        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
         Coordinate coordinate = new Coordinate(y, x);
-        Geometry point = geometryFactory.createPoint(coordinate);
-
-        MathTransform transform = CRS.findMathTransform(sourceCrs, targetCrs, true);
-        Geometry transFormedPoint = JTS.transform(point, transform);
-
-        return (Point) transFormedPoint;
+        Geometry point = GEOMETRY_FACTORY.createPoint(coordinate);
+        Geometry transformedPoint = JTS.transform(point, TRANSFORM);
+        return (Point) transformedPoint;
     }
 }
 
