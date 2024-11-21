b821eb97c0086abf4b16d806bea588c7c8405859
신동근 <tlsehdrms124@naver.com>
2024-11-21 10:37:11 +0900

    [BE] fix: 상권 분석 유동인구 및 편의 시설 관련 서비스 로직 Map Struct를 이용하여 리팩토링 진행

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/dto/response/CommercialPopulationResponse.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/dto/response/CommercialPopulationResponse.java
@@ -1,13 +1,16 @@
 package com.ssafy.backend.domain.commercial.dto.response;
 
 import com.ssafy.backend.domain.commercial.dto.info.CommercialPopulationInfo;
+import lombok.Builder;
 
 /**
  * 최종 사용자에게 반환될 상주 인구 응답 DTO입니다.
  */
+@Builder
 public record CommercialPopulationResponse(
-        CommercialPopulationInfo populationInfo,
-        Double malePercentage, // 남성 상주인구 퍼센트
-        Double femalePercentage // 여성 상주인구 퍼센트
+    CommercialPopulationInfo populationInfo,
+    Double malePercentage, // 남성 상주인구 퍼센트
+    Double femalePercentage // 여성 상주인구 퍼센트
 ) {
+
 }
