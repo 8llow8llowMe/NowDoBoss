95a9760ee78f035ca6b6d8537b84917af9334773
신동근 <tlsehdrms124@naver.com>
2024-11-21 14:23:57 +0900

    [BE] fix: Record Builder 패턴 적용

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/dto/response/CommercialAreaResponse.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/dto/response/CommercialAreaResponse.java
@@ -1,11 +1,15 @@
 package com.ssafy.backend.domain.commercial.dto.response;
 
+import lombok.Builder;
+
+@Builder
 public record CommercialAreaResponse(
-        String commercialCode,
-        String commercialCodeName,
-        String commercialClassificationCode,
-        String commercialClassificationCodeName,
-        Double centerLat,
-        Double centerLng
+    String commercialCode,
+    String commercialCodeName,
+    String commercialClassificationCode,
+    String commercialClassificationCodeName,
+    double centerLat,
+    double centerLng
 ) {
+
 }
