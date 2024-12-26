bdbb8adbe5130cea03584aabe4aae28c3383f3d1
신동근 <tlsehdrms124@naver.com>
2024-12-26 16:59:20 +0900

    [DATA] fix: 하둡 공통 관련 쉘 스크립트 수정 (SSH 키 설정)

--- CICD/DataProcessing/hadoop/common/init-ssh-keys.sh
+++ CICD/DataProcessing/hadoop/common/init-ssh-keys.sh
@@ -13,17 +13,3 @@ fi
 
 # 공유 디렉터리에 키 저장
 cp /root/.ssh/id_rsa.pub /shared_keys/$(hostname)_id_rsa.pub
-
-# 지정된 시간 동안 대기
-# sleep $1
-
-# authorized_keys 파일에 키 추가
-# echo "Adding $(hostname) key to authorized_keys"
-# cat /root/.ssh/$(hostname)_id_rsa.pub >> /root/.ssh/authorized_keys
-
-# authorized_keys 파일의 권한 설정
-# chmod 600 /root/.ssh/authorized_keys
-
-# SSH 데몬 실행
-# echo "SSH 데몬을 시작합니다."
-# /usr/sbin/sshd -D
