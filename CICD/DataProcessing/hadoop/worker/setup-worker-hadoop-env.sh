6be909c4c258a87eb5be1c16e198802dacb4e51f
신동근 <tlsehdrms124@naver.com>
2024-12-27 10:46:38 +0900

    [DATA] fix: 라즈베리파이5 CPU 아키텍처에 맞게끔 하둡 + 스파크 세팅 관련 arm64로 변경

--- CICD/DataProcessing/hadoop/worker/setup-worker-hadoop-env.sh
+++ CICD/DataProcessing/hadoop/worker/setup-worker-hadoop-env.sh
@@ -7,7 +7,7 @@ chmod 777 /usr/local/hadoop/hadoop_tmp/
 
 echo "worker의 환경변수를 설정합니다."
 # 환경변수 설정
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64' >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
 echo "/etc/hadoop 디렉토리를 확인합니다."
 # /etc/hadoop 디렉토리가 존재하지 않는다면 생성
