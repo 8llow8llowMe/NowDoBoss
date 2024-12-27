0f266a2a2972e7be986c34aadc50eef3581240e3
신동근 <tlsehdrms124@naver.com>
2024-12-27 10:46:38 +0900

    [DATA] fix: 라즈베리파이5 CPU 아키텍처에 맞게끔 하둡 + 스파크 세팅 관련 arm64로 변경

--- CICD/DataProcessing/spark/spark-env.sh
+++ CICD/DataProcessing/spark/spark-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # spark-env.sh
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export SPARK_WORKER_CORES=2
 export SPARK_WORKER_MEMORY=1g
