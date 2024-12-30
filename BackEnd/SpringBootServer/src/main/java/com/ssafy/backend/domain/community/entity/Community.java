3672586c1a5ab7141d2f51933c7e758d9633d872
신동근 <tlsehdrms124@naver.com>
2024-12-30 15:28:26 +0900

    [BE] fix: 빌드시 경고 (@Builder 관련 경고) 안뜨게끔 수정

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/community/entity/Community.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/community/entity/Community.java
@@ -3,12 +3,25 @@ package com.ssafy.backend.domain.community.entity;
 import com.ssafy.backend.domain.community.entity.enums.Category;
 import com.ssafy.backend.domain.member.entity.Member;
 import com.ssafy.backend.global.common.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.Comment;
-
-import java.util.ArrayList;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter
@@ -16,6 +29,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Community extends BaseEntity {
+
     @Id
     @Comment("커뮤니티 아이디")
     @Column(columnDefinition = "INT UNSIGNED")
@@ -45,10 +59,10 @@ public class Community extends BaseEntity {
     private int readCount;
 
     @OneToMany(mappedBy = "community", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Image> images = new ArrayList<>();
+    private List<Image> images;
 
     @OneToMany(mappedBy = "community", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comments> comments = new ArrayList<>();
+    private List<Comments> comments;
 
     public void read() {
         this.readCount++;
