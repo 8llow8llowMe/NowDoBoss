e9b6ab952a022297438789b901549dab2512feb9
신동근 <tlsehdrms124@naver.com>
2024-12-30 14:54:49 +0900

    [BE] fix: 회원 소셜 (Oauth 부분) 컨트롤러 필요없는 import 구문 삭제

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/controller/OAuthController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/controller/OAuthController.java
@@ -10,36 +10,44 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "소셜(회원)", description = "소셜(회원) 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/oauth")
+@Tag(name = "소셜(회원)", description = "소셜(회원) 관련 API 입니다.")
 public class OAuthController {
+
     private final OAuthService oAuthService;
 
     @Operation(
-            summary = "소셜 도메인을 통한 인증코드 가져오기",
-            description = "소셜 로그인 하기 전 인증코드를 가져와 소셜에 가입된 회원정보를 가져오는 기능입니다."
+        summary = "소셜 도메인을 통한 인증코드 가져오기",
+        description = "소셜 로그인 하기 전 인증코드를 가져와 소셜에 가입된 회원정보를 가져오는 기능입니다."
     )
     @GetMapping("/{oAuthDomain}")
-    public ResponseEntity<Message<String>> provideAuthCodeRequestUrlOAuth(@PathVariable OAuthDomain oAuthDomain) {
+    public ResponseEntity<Message<String>> provideAuthCodeRequestUrlOAuth(
+        @PathVariable OAuthDomain oAuthDomain) {
         String redirectUrl = oAuthService.provideAuthCodeRequestUrlOAuth(oAuthDomain);
         return ResponseEntity.ok().body(Message.success(redirectUrl));
     }
 
     @Operation(
-            summary = "소셜 로그인(회원가입)",
-            description = "인증코드를 통해 소셜 로그인(회원가입)을 하는 기능입니다."
+        summary = "소셜 로그인(회원가입)",
+        description = "인증코드를 통해 소셜 로그인(회원가입)을 하는 기능입니다."
     )
     @GetMapping("/{oAuthDomain}/login")
-    public ResponseEntity<Message<MemberLoginResponse>> loginOAuth(@PathVariable("oAuthDomain") OAuthDomain oAuthDomain,
-                                                                   @RequestParam("code") String authCode,
-                                                                   HttpServletResponse response) {
+    public ResponseEntity<Message<MemberLoginResponse>> loginOAuth(
+        @PathVariable("oAuthDomain") OAuthDomain oAuthDomain,
+        @RequestParam("code") String authCode,
+        HttpServletResponse response) {
         MemberLoginResponse loginResponse = oAuthService.loginOAuth(oAuthDomain, authCode);
         // JWT 토큰을 쿠키에 저장
-        Cookie accessTokenCookie = new Cookie("accessToken", loginResponse.tokenInfo().accessToken());
+        Cookie accessTokenCookie = new Cookie("accessToken",
+            loginResponse.tokenInfo().accessToken());
         accessTokenCookie.setPath("/");
         accessTokenCookie.setMaxAge(25200); // 4200분(25200초)으로 설정 (25200)
         response.addCookie(accessTokenCookie);
