32c7fc28a5e5e04123bfbb4ca217f85fd46fbb3e
신동근 <tlsehdrms124@naver.com>
2024-12-30 15:12:51 +0900

    [DATA] fix: Spark 코어수 및 메모리 설정 수정

--- CICD/DataProcessing/spark/spark-env.sh
+++ CICD/DataProcessing/spark/spark-env.sh
@@ -3,8 +3,8 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-export SPARK_WORKER_CORES=2
-export SPARK_WORKER_MEMORY=1g
+export SPARK_WORKER_CORES=4
+export SPARK_WORKER_MEMORY=4g
 export SPARK_MASTER_HOST=master1
 export SPARK_SUBMIT_OPTIONS="--add-opens java.base/sun.nio.ch=ALL-UNNAMED"
 export PYSPARK_PYTHON=/opt/venv/bin/python
