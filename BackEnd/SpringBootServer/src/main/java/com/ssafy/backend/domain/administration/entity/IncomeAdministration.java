8aa8711f5758e8c488f17cc7af21f6a00c9a49a1
신동근 <tlsehdrms124@naver.com>
2024-12-31 15:04:49 +0900

    [BE] feat: 소득소비_행정동 테이블 복합 인덱스 설정

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/administration/entity/IncomeAdministration.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/administration/entity/IncomeAdministration.java
@@ -1,7 +1,17 @@
 package com.ssafy.backend.domain.administration.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
 @Entity
@@ -10,10 +20,12 @@ import org.hibernate.annotations.Comment;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(indexes = {
-        @Index(name = "idx_period_code", columnList = "periodCode"),
-        @Index(name = "idx_administration_code", columnList = "administrationCode")
+    @Index(name = "idx_period_code", columnList = "periodCode"),
+    @Index(name = "idx_administration_code", columnList = "administrationCode"),
+    @Index(name = "idx_period_administration", columnList = "periodCode, administrationCode")
 })
 public class IncomeAdministration {
+
     @Id
     @Comment("소득소비_행정동 아이디")
     @Column(columnDefinition = "INT UNSIGNED")
