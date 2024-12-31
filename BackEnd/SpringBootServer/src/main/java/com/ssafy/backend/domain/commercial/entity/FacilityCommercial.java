d6ec2b7fe656a62ebf05003262989c1de78644bd
신동근 <tlsehdrms124@naver.com>
2024-12-31 15:17:22 +0900

    [BE] fix: 상권 도메인 관련 테이블들 복합 인덱스에 따른 단일 인덱스들 설정들 삭제

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/entity/FacilityCommercial.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/commercial/entity/FacilityCommercial.java
@@ -20,8 +20,6 @@ import org.hibernate.annotations.Comment;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(indexes = {
-    @Index(name = "idx_period_code", columnList = "periodCode"),
-    @Index(name = "idx_commercial_code", columnList = "commercialCode"),
     @Index(name = "idx_period_commercial", columnList = "periodCode, commercialCode")
 })
 public class FacilityCommercial {
