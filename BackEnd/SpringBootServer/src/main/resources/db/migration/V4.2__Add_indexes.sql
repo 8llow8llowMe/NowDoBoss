d4e4088ad8d881b4c1dfa7c10ba9a2c0ade79cbb
신동근 <tlsehdrms124@naver.com>
2024-12-31 17:46:07 +0900

    [BE] fix: Flyway 버전관리 쿼리 작성 진행중

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V4.2__Add_indexes.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V4.2__Add_indexes.sql
@@ -1,1 +1,18 @@
 -- V4.2__Add_indexes.sql
+
+-- IncomeAdministration 테이블 복합 인덱스 추가
+CREATE INDEX idx_period_administration
+    ON income_administration (period_code, administration_code);
+
+-- SalesAdministration 테이블 복합 인덱스 추가
+CREATE INDEX idx_period_administration_service
+    ON sales_administration (period_code, administration_code, service_code);
+
+-- StoreAdministration 테이블 단일 인덱스 추가
+CREATE INDEX idx_period_code
+    ON store_administration (period_code);
+CREATE INDEX idx_service_code
+    ON store_administration (service_code);
+CREATE INDEX idx_administration_code
+    ON store_administration (administration_code);
+
--- BackEnd/SpringBootServer/src/main/resources/db/migration/V4.3__Drop_indexes.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V4.3__Drop_indexes.sql
@@ -1,1 +1,25 @@
 -- V4.3__Drop_indexes.sql
+
+-- income_administration 테이블 단일 인덱스 삭제
+DROP INDEX idx_period_code ON income_administration;
+DROP INDEX idx_administration_code ON income_administration;
+
+-- facility_commercial 테이블 단일 인덱스 삭제
+DROP INDEX idx_period_code ON facility_commercial;
+DROP INDEX idx_commercial_code ON facility_commercial;
+
+-- foot_traffic_commercial 테이블 단일 인덱스 삭제
+DROP INDEX idx_period_code ON foot_traffic_commercial;
+DROP INDEX idx_commercial_code ON foot_traffic_commercial;
+
+-- income_commercial 테이블 단일 인덱스 삭제
+DROP INDEX idx_period_code ON income_commercial;
+DROP INDEX idx_commercial_code ON income_commercial;
+
+-- population_commercial 테이블 단일 인덱스 삭제
+DROP INDEX idx_period_code ON population_commercial;
+DROP INDEX idx_commercial_code ON population_commercial;
+
+-- sales_commercial 테이블 단일 인덱스 삭제
+DROP INDEX idx_period_code ON sales_commercial;
+DROP INDEX idx_commercial_code ON sales_commercial;
