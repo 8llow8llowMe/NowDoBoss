c4c3b12819e376cfc4fda00bfcc2745699142c1a
신동근 <tlsehdrms124@naver.com>
2025-01-07 13:58:25 +0900

    [BE] fix: Blue-Green 배포 전략 쉘 스크립트 수정

--- BackEnd/SpringBootServer/deploy_blue_green.sh
+++ BackEnd/SpringBootServer/deploy_blue_green.sh
@@ -12,12 +12,25 @@ if [ "$CURRENT_ENV" == "blue" ]; then
     # Green 배포
     docker-compose -f $DOCKER_COMPOSE_FILE --env-file $ENV_FILE up --build -d nowdoboss_springboot_green_service
 
-    echo "Green 환경으로 배포 완료."
+    echo "Green 환경으로 배포 완료. 기존 Blue 환경 종료 중..."
+    
+    # Blue 환경 종료
+    docker stop nowdoboss-backend-springboot-blue || true
+    docker rm nowdoboss-backend-springboot-blue || true
+
+    echo "Blue 환경 종료 완료."
 else
     echo "Green 환경 작동 중입니다. Blue 환경으로 배포합니다."
 
     # Blue 배포
     docker-compose -f $DOCKER_COMPOSE_FILE --env-file $ENV_FILE up --build -d nowdoboss_springboot_blue_service
 
-    echo "Blue 환경으로 배포 완료."
+
+    echo "Blue 환경으로 배포 완료. 기존 Green 환경 종료 중..."
+    
+    # Green 환경 종료
+    docker stop nowdoboss-backend-springboot-green || true
+    docker rm nowdoboss-backend-springboot-green || true
+
+    echo "Green 환경 종료 완료."
 fi
