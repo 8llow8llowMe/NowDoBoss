1bbb4d4f2c4fd41ee60b4a6bdb74a8bd0eb84158
신동근 <tlsehdrms124@naver.com>
2025-01-07 13:15:56 +0900

    [FE, BE] fix: Blue-Green 전략 관련 배포 쉘 스크립트 및 Nginx 설정 수정 (동적 프록시)

--- BackEnd/SpringBootServer/deploy_blue_green.sh
+++ BackEnd/SpringBootServer/deploy_blue_green.sh
@@ -7,33 +7,17 @@ ENV_FILE="src/main/resources/backend-env/.env-springboot"
 CURRENT_ENV=$(docker ps --filter "name=nowdoboss-backend-springboot-blue" --filter "status=running" --format "{{.Names}}" | grep blue && echo "blue" || echo "green")
 
 if [ "$CURRENT_ENV" == "blue" ]; then
-    echo "Blue 환경 작동 중 입니다. Green 환경으로 배포하겠습니다."
-    
+    echo "Blue 환경 작동 중입니다. Green 환경으로 배포합니다."
+
     # Green 배포
     docker-compose -f $DOCKER_COMPOSE_FILE --env-file $ENV_FILE up --build -d nowdoboss_springboot_green_service
 
-    # Nginx 설정 변경
-    sed -i 's/backend_blue/backend_green/' /etc/nginx/conf.d/default.conf
-    nginx -s reload
-
-    # Blue 종료
-    docker stop nowdoboss-backend-springboot-blue || true
-    docker rm nowdoboss-backend-springboot-blue || true
-
-    echo "Green 환경으로 변경 완료"
+    echo "Green 환경으로 배포 완료."
 else
-    echo "Green 환경 작동 중 입니다. Blue 환경으로 배포하겠습니다."
+    echo "Green 환경 작동 중입니다. Blue 환경으로 배포합니다."
 
     # Blue 배포
     docker-compose -f $DOCKER_COMPOSE_FILE --env-file $ENV_FILE up --build -d nowdoboss_springboot_blue_service
 
-    # Nginx 설정 변경
-    sed -i 's/backend_green/backend_blue/' /etc/nginx/conf.d/default.conf
-    nginx -s reload
-
-    # Green 종료
-    docker stop nowdoboss-backend-springboot-green || true
-    docker rm nowdoboss-backend-springboot-green || true
-
-    echo "Blue 환경으로 변경 완료"
+    echo "Blue 환경으로 배포 완료."
 fi
