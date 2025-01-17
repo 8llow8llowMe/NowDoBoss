13a7289ac669373dcccc7ed0fa1dc8ba8b0625d8
신동근 <tlsehdrms124@naver.com>
2025-01-17 17:52:41 +0900

    [BE] fix: 추천 로직 수정 (Parquet 파일 없으면 생성하게끔 하고 그걸 계속 재사용하는 식으로 수정)

--- BackEnd/FastApiServer/recommendation_engine.py
+++ BackEnd/FastApiServer/recommendation_engine.py
@@ -2,10 +2,11 @@ from pyspark.sql.functions import col, explode, desc, lit, when
 from pyspark.ml.recommendation import ALS, ALSModel
 import mongoDB
 import pandas as pd
+from pyspark.sql.utils import AnalysisException
 
+CSV_PATH = "hdfs://master1:9000/data/commercial_data.csv"     # CSV 파일
+PARQUET_PATH = "hdfs://master1:9000/data/commercial_data.parquet"
 MODEL_PATH = "hdfs://master1:9000/models/recommendation_model"
-# CSV 대신 parquet로 변경된 경로
-COMMERCIAL_DATA_PATH = "hdfs://master1:9000/data/commercial_data.parquet"
 
 ACTION_WEIGHTS = {"click": 2, "search": 4, "analysis": 7, "save": 10}
 
@@ -13,12 +14,31 @@ ACTION_WEIGHTS = {"click": 2, "search": 4, "analysis": 7, "save": 10}
 cached_commercial_data = None
 cached_als_model = None  # ALS 모델 캐싱
 
+def path_exists(spark, path):
+    try:
+        spark.read.parquet(path).limit(1).collect()
+        return True
+    except AnalysisException as ae:
+        if "Path does not exist" in str(ae):
+            return False
+        raise ae
+
 def load_commercial_data(spark):
     global cached_commercial_data
-    if cached_commercial_data is None:
-        print("Parquet 상권 데이터 로드 및 캐싱 중...")
-        cached_commercial_data = spark.read.parquet(COMMERCIAL_DATA_PATH).cache()
-        print("Parquet 상권 데이터 캐싱 완료")
+    if cached_commercial_data is not None:
+        return cached_commercial_data
+
+    # Parquet 파일 유무 확인
+    if not path_exists(spark, PARQUET_PATH):
+        print(f"[load_commercial_data] Parquet 파일이 없어 CSV({CSV_PATH})에서 변환을 시작합니다.")
+        df = spark.read.csv(CSV_PATH, header=True, inferSchema=True)
+        df.write.parquet(PARQUET_PATH)
+        print("[load_commercial_data] CSV -> Parquet 변환 완료.")
+
+    print("[load_commercial_data] Parquet 상권 데이터 로드 및 캐싱 중...")
+    cached_commercial_data = spark.read.parquet(PARQUET_PATH).cache()
+    print("[load_commercial_data] Parquet 상권 데이터 캐싱 완료")
+
     return cached_commercial_data
 
 async def recommend(spark, user_id, background_tasks):
