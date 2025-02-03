dfbb469c8f9414bebcc02c0f9863da2aeefaf99f
choiseongho <chltjdgh3@naver.com>
2025-02-04 00:37:43 +0900

    feat: 푸터 추가 및 헤더, 푸터 구조 변경

--- FrontEnd/src/util/HeaderAndFooter.tsx
+++ FrontEnd/src/util/HeaderAndFooter.tsx
@@ -1,8 +1,9 @@
 import { matchPath, useLocation } from 'react-router-dom'
 import Header from '@src/containers/header/HeaderContainer'
+import Footer from '@src/containers/footer/FooterContainer'
 
-// 헤더 사용하지 않는 페이지 예외 적용
-const ConditionalHeader = () => {
+// 헤더와 푸터를 사용하지 않는 페이지 예외 적용
+const HeaderAndFooter = ({ content }: { content: string }) => {
   const location = useLocation()
   const currentPathname = location.pathname
 
@@ -14,7 +15,7 @@ const ConditionalHeader = () => {
   ) {
     return null
   }
-  return <Header />
+  return content === 'header' ? <Header /> : <Footer />
 }
 
-export default ConditionalHeader
+export default HeaderAndFooter
