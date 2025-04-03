055d2d5704e0d45d3fb8698dd1ed79e3110253c0
신동근 <tlsehdrms124@naver.com>
2025-04-03 15:57:15 +0900

    [BE] fix: 해당 인터셉터 리팩토링

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/aspect/interceptor/PublicEndpointInterceptor.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/aspect/interceptor/PublicEndpointInterceptor.java
@@ -8,15 +8,22 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 public class PublicEndpointInterceptor implements HandlerInterceptor {
 
+    private static final String ATTRIBUTE_NAME = "IS_PUBLIC";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-        Object handler) throws Exception {
-        if (handler instanceof HandlerMethod handlerMethod) {
-            if (handlerMethod.getMethodAnnotation(PublicEndpoint.class) != null ||
-                handlerMethod.getBeanType().getAnnotation(PublicEndpoint.class) != null) {
-                request.setAttribute("IS_PUBLIC", true);
-            }
+        Object handler) {
+        if (!(handler instanceof HandlerMethod handlerMethod)) {
+            return true;
         }
+
+        boolean isPublic = handlerMethod.getMethodAnnotation(PublicEndpoint.class) != null
+            || handlerMethod.getBeanType().isAnnotationPresent(PublicEndpoint.class);
+
+        if (isPublic) {
+            request.setAttribute(ATTRIBUTE_NAME, true);
+        }
+
         return true;
     }
 }
