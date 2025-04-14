fecd5fed7d82595e40c2e319ff103455f9527043
신동근 <tlsehdrms124@naver.com>
2025-04-14 14:25:00 +0900

    [BE] fix: Spring Boot 백엔드 서비스 무중단 배포 쉘 스크립트 수정

--- BackEnd/SpringBootServer/deploy_blue_green.sh
+++ BackEnd/SpringBootServer/deploy_blue_green.sh
@@ -46,15 +46,18 @@ if [ "$CURRENT_ENV" == "blue" ]; then
     # 1-1) 새로운 Green 컨테이너의 헬스체크
     wait_for_container_health $SPRING_BOOT_INTERNAL_PORT
 
-    # 2) Blue 컨테이너 중지 및 제거
-    echo "[CLEANUP] 기존 Blue 컨테이너 종료 및 제거..."
-    docker stop -t 30 nowdoboss-backend-springboot-blue || true
-    docker rm nowdoboss-backend-springboot-blue || true
+    # 2) 이전 Blue 컨테이너에서 alias 제거 
+    docker network disconnect nowdoboss-net nowdoboss-backend-springboot-blue || true
     
     # 3) 새로 띄워진 Green 컨테이너의 기존 네트워크 연결을 해제하고, alias를 사용해 도메인 이름(nowdoboss-backend-springboot)으로 네트워크에 다시 연결
     echo "[NETWORK] Green 컨테이너 네트워크 alias 변경 중..."
-    docker network disconnect nowdoboss-net nowdoboss-backend-springboot-green
+    docker network disconnect nowdoboss-net nowdoboss-backend-springboot-green  
     docker network connect --alias nowdoboss-backend-springboot nowdoboss-net nowdoboss-backend-springboot-green
+
+    # 4) Blue 컨테이너 중지 및 제거
+    echo "[CLEANUP] 기존 Blue 컨테이너 종료 및 제거..."
+    docker stop -t 30 nowdoboss-backend-springboot-blue || true
+    docker rm nowdoboss-backend-springboot-blue || true
 
     echo "[SUCCESS] Green 환경 전환 완료! (alias nowdoboss-backend-springboot -> Green)"
 
@@ -68,15 +71,18 @@ else
     # 1-1) 새로운 Blue 컨테이너의 헬스체크
     wait_for_container_health $SPRING_BOOT_INTERNAL_PORT
 
-    # 2) Green 컨테이너 중지 및 제거
-    echo "[CLEANUP] 기존 Green 컨테이너 종료 및 제거..."
-    docker stop -t 30 nowdoboss-backend-springboot-green || true
-    docker rm nowdoboss-backend-springboot-green || true
-
+    # 2) 이전 Green 컨테이너에서 alias 제거 
+    docker network disconnect nowdoboss-net nowdoboss-backend-springboot-green || true
+    
     # 3) 새로 띄워진 Blue 컨테이너의 기존 네트워크 연결을 해제하고, alias를 사용해 도메인 이름(nowdoboss-backend-springboot)으로 네트워크에 다시 연결
     echo "[NETWORK] Blue 컨테이너 네트워크 alias 변경 중..."
     docker network disconnect nowdoboss-net nowdoboss-backend-springboot-blue
     docker network connect --alias nowdoboss-backend-springboot nowdoboss-net nowdoboss-backend-springboot-blue
+
+    # 4) Green 컨테이너 중지 및 제거
+    echo "[CLEANUP] 기존 Green 컨테이너 종료 및 제거..."
+    docker stop -t 30 nowdoboss-backend-springboot-green || true
+    docker rm nowdoboss-backend-springboot-green || true
 
     echo "[SUCCESS] Blue 환경 전환 완료! (alias nowdoboss-backend-springboot -> Blue)"
 fi
