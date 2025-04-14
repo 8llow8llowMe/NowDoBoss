6431d289872dda8dc0270710004c8ecd5da7fc65
신동근 <tlsehdrms124@naver.com>
2025-04-14 13:58:10 +0900

    [BE] fix: 무중단 배포시 DB Connection 관련 오류로 인한 설정 수정 및 추가

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/shutdown/GracefulShutdownHandler.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/shutdown/GracefulShutdownHandler.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend.global.component.shutdown;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,23 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class GracefulShutdownHandler {
 
+    private final HikariDataSource dataSource;
+
     @PreDestroy
     public void onShutdown() {
         log.info("[SHUTDOWN] 애플리케이션 종료 감지됨. Graceful Shutdown 시작...");
+
+        try {
+            if (dataSource != null && !dataSource.isClosed()) {
+                log.info("[SHUTDOWN] HikariCP 커넥션 풀을 종료합니다...");
+                dataSource.close();  // 여기서 DB 커넥션 정리
+                log.info("[SHUTDOWN] 커넥션 풀 종료 완료.");
+            }
+        } catch (Exception e) {
+            log.error("[SHUTDOWN] 커넥션 풀 종료 중 예외 발생", e);
+        }
+
+        log.info("[SHUTDOWN] Graceful Shutdown 작업 완료.");
     }
 
 }
