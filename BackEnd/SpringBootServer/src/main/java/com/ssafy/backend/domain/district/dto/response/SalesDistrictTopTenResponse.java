5824b62ee77cfbe8e86889f21acdedc978f14d2f
신동근 <tlsehdrms124@naver.com>
2025-04-15 15:16:40 +0900

    [BE] fix: 구글 스타일 적용하여 리팩토링 (간격)

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/district/dto/response/SalesDistrictTopTenResponse.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/district/dto/response/SalesDistrictTopTenResponse.java
@@ -4,11 +4,11 @@ import lombok.Builder;
 
 @Builder
 public record SalesDistrictTopTenResponse(
-        String districtCode,
-        String districtCodeName,
-        Long total,
-        Double totalRate,
-        int level
-
+    String districtCode,
+    String districtCodeName,
+    Long total,
+    Double totalRate,
+    int level
 ) {
+
 }
